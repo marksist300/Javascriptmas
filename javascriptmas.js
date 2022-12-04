64632bd8549a49ae95df95ffb30f05074a1c1b52
Mark Linn <marktlinn@gmail.com>
2022-12-04 11:05:20 +0100

    add day 4 challenge completed

--- javascriptmas.js
+++ javascriptmas.js
@@ -133,3 +133,31 @@ and for supper I want usually want pizza 🍕.
 // <br/>
 // Finally, for dinner my favourite meal has got to be ${supper}
 // </p>`
+
+// Day 4
+
+/* Whispering function 
+Write a function `whisper` that takes in a sentence 
+and returns a new sentence in all lowercase letters with
+"shh..." at the beginning. 
+
+The function should also remove an exclamation point
+at the end of the sentence, if there is one. 
+
+Example 
+input: "The KITTENS are SLEEPING!"
+output: "shh... the kittens are sleeping"
+
+Hint: endsWith and slice
+*/
+
+const whisper = str => {
+  return str.endsWith("!")
+    ? `shh... ${str.slice(0, [str.length - 1]).toLowerCase()}`
+    : str;
+};
+
+console.log(whisper("PLEASE STOP SHOUTING."));
+console.log(whisper("MA'AM, this is a Wendy's!"));
+console.log(whisper("The KITTENS are SLEEPING!"));
+console.log(whisper("Wow!"));
