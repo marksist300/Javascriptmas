8899bd5095b71dfa2dcac737eac374c696d7b2e7
Mark Linn <marktlinn@gmail.com>
2022-12-06 08:30:15 +0100

    add: dialy challenge completed

--- javascriptmas.js
+++ javascriptmas.js
@@ -221,4 +221,33 @@ function getSaleItems(data) {
   return allCandy.map(({ item, price }) => ({ item, price }));
 }
 
-console.log(getSaleItems(products));
+// console.log(getSaleItems(products));
+
+//Day 6
+
+function getRandomNumberOfTacos() {
+  /*
+  Make this function return an array that contains 
+  between one and ten taco emojis 🌮
+  Use the following JavaScript concepts:
+      - Math.random()
+      - Math.floor()
+      - new Array()
+      - Array.fill()
+  */
+
+  const elemCount = Math.floor(Math.random() * 10) + 1;
+  const tacoTray = new Array(elemCount).fill("🌮");
+
+  return tacoTray; // replace this empty tray array
+}
+
+function putTacosOnTray() {
+  return getRandomNumberOfTacos()
+    .map(function (taco) {
+      return `<div class="taco">${taco}</div>`;
+    })
+    .join("");
+}
+
+// document.getElementById("tray").innerHTML = putTacosOnTray();
