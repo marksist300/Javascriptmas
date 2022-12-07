693258abf4ec89c1e1b0aea108a624d76edaa01e
Mark Linn <marktlinn@gmail.com>
2022-12-07 10:34:45 +0100

    add: challenge for day 7 done

--- javascriptmas.js
+++ javascriptmas.js
@@ -251,3 +251,22 @@ function putTacosOnTray() {
 }
 
 // document.getElementById("tray").innerHTML = putTacosOnTray();
+
+// Day 7
+
+/* Alternating Caps 
+ Write a function that takes in a string of letters
+ and returns a sentence in which every other letter is capitalized.
+
+Example input: "I'm so happy it's Monday"
+Example output: "I'M So hApPy iT'S MoNdAy"
+*/
+
+function altCaps(str) {
+  return str
+    .split("")
+    .map((letter, index) => (index % 2 === 0 ? letter.toUpperCase() : letter))
+    .join("");
+}
+
+// console.log(altCaps("When you visit Portland you have to go to VooDoo Donuts"));
