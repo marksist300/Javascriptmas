fa758b6c1dfa46798e77158164a3e650e652c301
Mark Linn <marktlinn@gmail.com>
2022-12-10 11:06:57 +0100

    add daily challenge update

--- javascriptmas.js
+++ javascriptmas.js
@@ -366,3 +366,28 @@ function toTitleCase(str) {
 // Test your functions
 // console.log(capitalizeWord("pumpkin"));
 // console.log(toTitleCase("pumpkin pranced purposefully across the pond"));
+
+// Day 10
+
+function sortByLength(strs) {
+  return strs.sort((a, b) => a.length - b.length);
+}
+
+/**
+ * Tests
+ */
+// describe('sortByLength()', () => {
+//   it('sorts the strings from shortest to longest', () => {
+//       // arrange
+//       const strs = ["abc", "", "aaa", "a", "zz"];
+
+//       // act
+//       const result = sortByLength(strs);
+
+//       // log
+//       console.log("result: ", result);
+
+//       // assert
+//       expect(result).toEqual(["", "a", "zz", "abc", "aaa"]);
+//   });
+// });
