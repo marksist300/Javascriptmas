b3aef334f6255f6183dffb86f49aadc9c1bced93
Mark Linn <marktlinn@gmail.com>
2022-12-16 10:39:07 +0100

    add: daily challenge completed

--- javascriptmas.js
+++ javascriptmas.js
@@ -552,3 +552,33 @@ function isPalindrome(str) {
   const checkWord = str.toLowerCase().split("").reverse().join("");
   return checkWord === str ? true : false;
 }
+
+// Day 16
+
+function insertDashes(arr) {
+  return arr
+    .split(" ")
+    .map(elem => [...elem].join("-"))
+    .join(" ");
+}
+
+/**
+ * Test Suite
+ */
+/*
+describe('insertDashes()', () => {
+  it('insert dashes in between chars', () => {
+      // arrange
+      const value = "aba caba";
+      
+      // act
+      const result = insertDashes(value);
+
+      // log
+      console.log("result: ", result);
+      
+      // assert
+      expect(result).toBe("a-b-a c-a-b-a");
+  });
+});
+*/
