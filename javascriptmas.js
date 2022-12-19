8a69fd0f5bf7cd7e3c577a32797a9df73b92ca23
Mark Linn <marktlinn@gmail.com>
2022-12-19 15:06:20 +0100

    add: daily challenge completed

--- javascriptmas.js
+++ javascriptmas.js
@@ -699,3 +699,42 @@ function candies(children, candy) {
 //       expect(result).toBe(15);
 //   });
 // });
+
+// Day 19
+function centuryFromYear(num) {
+  const century = Math.floor(String(num).slice(0, 2));
+  return century === 19 ? 20 : century;
+}
+
+/**
+ * Test Suite
+ */
+// describe('centuryFromYear()', () => {
+//   it('returns current century', () => {
+//       // arrange
+//       const year = 1905;
+
+//       // act
+//       const result = centuryFromYear(year);
+
+//       // log
+//       console.log("result 1: ", result);
+
+//       // assert
+//       expect(result).toBe(20);
+//   });
+
+//   it('returns current century for edge case of start of century', () => {
+//       // arrange
+//       const year = 1700;
+
+//       // act
+//       const result = centuryFromYear(year);
+
+//       // log
+//       console.log("result 2: ", result);
+
+//       // assert
+//       expect(result).toBe(17);
+//   });
+// });
