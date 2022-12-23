64e4b3a0314ae06d719c99d1266dda2868b8145c
Mark Linn <marktlinn@gmail.com>
2022-12-23 11:09:55 +0100

    daily challenge done

--- javascriptmas.js
+++ javascriptmas.js
@@ -852,3 +852,28 @@ be generated for each table value. Here is an example:
 5. Set the innerHTML content of the tables <section> 
 to the displayTables function call.
 */
+
+// Day 23
+
+/*
+    You're online shopping for holiday gifts, but money is tight
+    so we need to look at the cheapest items first. 
+    Use the built in sort() method to write a function that returns a new array of
+    products sorted by price, cheapest to most expensive. 
+    
+    Then log the item and the price to the console: 
+    
+    💕,0
+    🍬,0.89
+    🍫,0.99
+    🧁,0.99
+    📚,0.99
+    ... continued
+*/
+
+function sortProducts(data) {
+  return data.sort((a, b) => a.price - b.price);
+}
+
+// const listByCheapest = sortProducts(products);
+// console.log(listByCheapest);
